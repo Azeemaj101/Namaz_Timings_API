e307ba7d4757cec856025d0bd65bde5b8c032217
Muhammad Azeem <61928905+Azeemaj101@users.noreply.github.com>
2021-07-29 12:38:24 +0500

    Adding Update 1.0

---
+++ src/index.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const bodyParser = require('body-parser');
+const request = require("request");
+app.use(bodyParser.urlencoded({ extended: false }));
+let Port = process.env.PORT || 8000;
+
+let temp_path = path.join(__dirname, "..", "templates", "views");
+const static_path = path.join(__dirname, "../templates/public");
+app.use(express.static(static_path));
+
+app.set("view engine", "ejs");
+app.set("views", temp_path);
+
+app.get("/", (req, res) => {
+    let country = "";
+    let city = "";
+    request(`https://restcountries.eu/rest/v2/`, (err, resp) => {
+        if (err) {
+            res.status(404).render('404', {
+                err: "Server Error"
+            });
+        } else {
+            let API_Country = JSON.parse(resp.body);
+            // let API_City = [API_Ciy];
+            country = API_Country;
+        }
+        res.render("index", {
+            country1: country
+        });
+    });
+
+
+
+});
+app.post('/', (req, res) => {
+    const city = req.body.city;
+    const country = req.body.country;
+    if (city === "No" || country === "No") {
+        res.status(404).render('404', {
+            err: "Empty Field"
+        });
+    } else {
+        let date = new Date();
+        let today = new Date();
+        var dd = today.getDate();
+
+        var mm = today.getMonth() + 1;
+        var yyyy = today.getFullYear();
+        if (dd < 10) {
+            dd = '0' + dd;
+        }
+
+        if (mm < 10) {
+            mm = '0' + mm;
+        }
+        today = dd + '-' + mm + '-' + yyyy;
+        request(`https://api.aladhan.com/v1/calendarByCity?city=${city}&country=${country}&method=2&month=${date.getMonth}&year=${date.getFullYear}`, (err, resp) => {
+            if (err) {
+                res.status(500).render('500', {
+                    err: "Server Error"
+                });
+            } else {
+                let v = JSON.parse(resp.body);
+                res.render('namaz_page', {
+                    city2: city,
+                    city1: `${city}, ${country}`,
+                    obj: v,
+                    date1: today,
+                    year: yyyy
+                });
+            }
+        });
+    }
+});
+app.listen(Port, () => {
+    console.log(`${Port}`);
+});
